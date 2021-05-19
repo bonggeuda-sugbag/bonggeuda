51dee3ab8047b9961b4ed344b8d14dc278456a3d
haeseung-lee <dlgotmd93@gmail.com>
2021-05-19 14:55:16 +0900

    0519..타입별 숙소조회(사진,검색,정렬 미완)

--- src/com/bonggeuda/sugbag/service/BookService.java
+++ src/com/bonggeuda/sugbag/service/BookService.java
@@ -5,6 +5,7 @@ import static com.bonggeuda.sugbag.jdbc.JDBCTemplate.getConnection;
 
 import java.sql.Connection;
 import java.util.List;
+import java.util.Map;
 
 import com.bonggeuda.sugbag.model.dao.BookDAO;
 import com.bonggeuda.sugbag.model.dto.AccomoInfoDTO;
@@ -17,6 +18,11 @@ public class BookService {
 		bookDao = new BookDAO();
 	}
 
+	/**
+	 * 숙소리스트 검색
+	 * @param type 숙소타입(호텔,펜션,게스트하우스)
+	 * @return 선택한 타입의 숙소리스트
+	 */
 	public List<AccomoInfoDTO> selectAccomoList(String type) {
 		
 		Connection con = getConnection();
@@ -25,6 +31,21 @@ public class BookService {
 		accomoList = bookDao.selectAccomoList(con, type);
 		close(con);
 		return accomoList;
+	}
+
+	/**
+	 * 최저가 검색
+	 * @param type 숙소타입
+	 * @return 숙소의 최저값
+	 */
+	public List<Map> selectMinPrice(String type) {
+		
+		Connection con = getConnection();
+		List<Map> accomoPriceNStar = null;
+		accomoPriceNStar = bookDao.selectMinPrice(con, type);
+		
+		close(con);
+		return accomoPriceNStar;
 	}
 
 }
